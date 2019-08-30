2bb2463d461891677bea8eb05896d6d1f37e82d3
Mike-T-Anderson <50447482+Mike-T-Anderson@users.noreply.github.com>
2019-08-30 14:28:30 +1000

    Create Uninstall.ps1

---
+++ Software/Uninstall.ps1
@@ -0,0 +1,80 @@
+<#
+.Synopsis
+   Uninstall Programs 
+.DESCRIPTION
+   Find Uninstall Strings in the Registry and Uninstall Program.
+.EXAMPLE
+   Invoke-Uninstall "Revit 2018"
+.EXAMPLE
+   Invoke-Uninstall "Revit 2018" -passive 
+   (this will make the uninstaller Passive with Prompts Default is silent)
+.AUTHOR 
+    Date: 30/08/2019
+    Michael Anderson - https://github.com/Mike-T-Anderson/
+#>
+Function Invoke-Uninstall {
+
+    Param(
+        
+        [Parameter(Mandatory = $true, Position = 0)][string] $App,
+        [Parameter(Mandatory=$false)][Switch]$Passive
+        
+         )
+          
+# ------------------------------------------[ Variables ] --------------------------------- #
+
+$app = $App.Replace(' ','*')
+$productNames = "*$App*"
+
+if($passive)
+    {$Method = "passive"}
+else{$Method = "quiet"}
+     
+# ------------------------------------------[ Find Uninstall Key, String] --------------------------------- #
+
+$UninstallKeys = @(
+                    'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall',
+                    'HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall'
+                  )
+
+$uninstallKeys = foreach ($key in (Get-ChildItem $UninstallKeys) ) {
+
+    foreach ($product in $productNames) {
+        if ($key.GetValue("DisplayName") -like "$product") {
+            [pscustomobject]@{
+                KeyName         = $key.Name.split('\')[-1];
+                DisplayName     = $key.GetValue("DisplayName");
+                UninstallString = $key.GetValue("UninstallString");
+                Publisher       = $key.GetValue("Publisher");
+            }
+        }
+    }
+}
+
+# ------------------------------------------ [Uninstall Software ]  ------------------------------------------ #
+
+foreach ($key in $uninstallkeys) {
+
+    $guid = $key.keyname
+  
+    if ($guid.StartsWith("{")) {
+        $uninstallString = "MsiExec.exe /X $guid /$method /norestart"
+                        
+        #Uncomment to Run
+        cmd /c "$uninstallString"
+    }
+    else {
+        if ($key.UninstallString -ne $null) {
+           
+            $uninstallString = $key.UninstallString
+       
+            if ($key.UninstallString -notlike '"*') {
+                $uninstallString = $uninstallString.Replace('C:\', '"C:\')
+                $uninstallString = $uninstallString.Replace('.exe', '.exe"')
+            }                
+        
+            cmd /c $uninstallString 
+        }
+    }
+}
+}
